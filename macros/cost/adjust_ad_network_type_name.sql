e584360f5a14b6f2073f3e944c75f4a5e3619b74
phucdm@volio.vn <phucdm@volio.vn>
2023-07-05 07:20:19 +0000

    add macro

---
+++ macros/cost/adjust_ad_network_type_name.sql
@@ -0,0 +1,11 @@
+{% macro adjust_ad_network_type_name(column_name) %}
+    case
+        when {{column_name}} = 'SEARCH' then 'Google search'
+        when {{column_name}} = 'SEARCH_PARTNERS' then 'Search partners'
+        when {{column_name}} = 'CONTENT' then 'Display Network'
+        when {{column_name}} = 'YOUTUBE_SEARCH' then 'YouTube Search'
+        when {{column_name}} = 'YOUTUBE_WATCH' then 'YouTube Videos'
+        when {{column_name}} = 'MIXED' then 'Cross-network'
+        else lower({{column_name}})
+    end as ad_network_type
+{%- endmacro %}
---
+++ macros/revenue/generate_union_query_for_mutiple_tables_from_same_dataset.sql
@@ -0,0 +1,38 @@
+{% macro generate_union_query_for_mutiple_tables_from_same_dataset(project_id, dataset_id,no_month=none) %}
+
+    {% set from_clause = [project_id,dataset_id,'INFORMATION_SCHEMA.TABLES'] | join('.') %}
+
+    {% set list_table_name_sql %}
+        SELECT table_name 
+        from {{from_clause}} 
+        order by table_name desc 
+        {% if no_month is not none %}
+        limit {{no_month}}
+        {% endif %}
+    {% endset %}
+
+    {% if execute %}
+    {% set results = run_query(list_table_name_sql) %}
+    {% set list_table_name = results.columns[0].values()  %}
+    {% else %}
+    {% set list_table_name = [] %}
+    {% endif %}
+
+    {% for name in list_table_name %}
+        select 
+            Order_Number, 
+            Order_Charged_Date, 
+            Order_Charged_Timestamp,
+            Financial_Status, 
+            Product_Title, 
+            Product_ID, 
+            Product_Type,
+            SKU_ID, 
+            Currency_of_Sale, 
+            Item_Price,
+            Country_of_Buyer
+        from {{[project_id,dataset_id,name] | join('.')}}
+    {% if not loop.last %} union all
+    {% endif %}
+    {% endfor %}
+{% endmacro %}
