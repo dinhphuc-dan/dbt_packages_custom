4be7633c7f839ae183d77d29fcf2d8a434d8a260
phucdm@volio.vn <phucdm@volio.vn>
2023-07-13 05:00:11 +0000

    add new macro

--- macros/firebase/clean_firebase_device_os_version_data.sql
+++ macros/firebase/clean_firebase_device_os_version_data.sql
@@ -1,10 +1,15 @@
-{% macro clean_firebase_device_os_version_data(column_name) %}
-    case when
-        if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}'), '[.]'), concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}')), if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'), '[.]'),concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'),'.0'),concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '[0-9]{1,2}'),'.0.0'))) is null
-        or 
-        if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}'), '[.]'), concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}')), if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'), '[.]'),concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'),'.0'),concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '[0-9]{1,2}'),'.0.0'))) = ''
-    then 'unknown'
-    else
-        if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}'), '[.]'), concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}')), if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'), '[.]'),concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'),'.0'),concat(REGEXP_EXTRACT({{column_name}}, '([a-zA-Z]+)'),' ',REGEXP_EXTRACT({{column_name}}, '[0-9]{1,2}'),'.0.0')))
-    end
+{% macro clean_firebase_device_os_version_data(column_name, condition = var('device_os_system') ) %}
+    
+    {% if condition == 'android_only' %}
+        {{clean_android_device_os_version_data(column_name)}}
+    {% elif condition == 'both_android_and_ios' or condition == 'ios_only' %}
+        case when
+            if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}'), '[.]'), concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}')), if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'), '[.]'),concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'),'.0'),concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '[0-9]{1,2}'),'.0.0'))) is null
+            or 
+            if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}'), '[.]'), concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}')), if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'), '[.]'),concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'),'.0'),concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '[0-9]{1,2}'),'.0.0'))) = ''
+        then 'unknown'
+        else
+            if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}'), '[.]'), concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9].[0-9]){1}')), if(REGEXP_CONTAINS(REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'), '[.]'),concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '([0-9]{1,2}.[0-9]){1}'),'.0'),concat(device.operating_system,' ',REGEXP_EXTRACT({{column_name}}, '[0-9]{1,2}'),'.0.0')))
+        end
+    {% endif %}
 {% endmacro %}
