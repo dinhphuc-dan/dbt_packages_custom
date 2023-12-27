2fe28074bd0c080f3e4e61994c30eeafc058751c
phucdm@volio.vn <phucdm@volio.vn>
2023-12-27 04:11:44 +0000

    restucture macro folder merge all macro for event_params into only one macro, so called raw_firebase_events_params

--- macros/macro_generate_model/firebase/raw_firebase_events_params.sql
+++ macros/macro_generate_model/firebase/raw_firebase_events_params.sql
@@ -1,5 +1,7 @@
-_{% macro generate_query_call_event_without_param(event_name_list_as_dict, ref_model_name) %}
-    {%- set event_name_list = [] -%}
+{%- macro raw_firebase_events_params(event_name_list_as_dict, ref_raw_model, ref_screen_button_model) -%}
+{{ config(materialized='ephemeral') }}
+
+{%- set event_name_list = [] -%}
         {%- for item in event_name_list_as_dict -%}
             {%- for key, value in item.items() %}
             {%-do event_name_list.append("'"~ key ~"'")-%}
@@ -21,15 +23,12 @@ _{% macro generate_query_call_event_without_param(event_name_list_as_dict, ref_m
         {%- if var('device_os_system') == 'both_android_and_ios' %}
         platform,
         {% endif -%}
-    from {{ ref_model_name }}
+    from {{ ref_raw_model }}
     where
         event_name in {{ '(' ~ event_name_list | join(',') ~ ')'}}
-    )
-{% endmacro%}
+    ),
 
-{% macro generate_query_call_event_with_param_then_join(event_name_list_as_dict, ref_model_name) %}
-    {%- set event_name_list = [] -%}
-    {%- set param_key_list = [] -%}
+{%- set param_key_list = [] -%}
 
         {%- for item in event_name_list_as_dict -%}
             {%- for key, value in item.items() -%}
@@ -48,7 +47,7 @@ _{% macro generate_query_call_event_without_param(event_name_list_as_dict, ref_m
                 event_name,
                 user_id,
                 COALESCE(event_params.value.string_value, cast(event_params.value.int_value as STRING), cast(event_params.value.float_value as STRING), cast(event_params.value.double_value as STRING), 'unknown') as {{param_key}}
-            from {{ ref_model_name }}
+            from {{ ref_raw_model }}
             left join unnest(event_params) as event_params
             where
                 event_name in {{ '(' ~ event_name_list | join(',') ~ ')'}}
@@ -70,5 +69,29 @@ _{% macro generate_query_call_event_without_param(event_name_list_as_dict, ref_m
     left join  {{ 't_' ~ param_key}}
     using(date, event_timestamp, event_name, user_id)
     {%-endfor-%}
-    )
-{% endmacro%}
+    ),
+
+d_eventName as 
+(
+    select * from {{ref_screen_button_model}}
+),
+t_final_2 as 
+(
+    select 
+        t_final.* except(event_name),
+        t_final.event_name as event_name_in_firebase,
+        ifnull(d_eventName.screen_feature_button_name, t_final.event_name) as event_name
+    from t_final
+    left join d_eventName 
+    on t_final.event_name = d_eventName.firebase_event_name
+
+),
+final as -- new colum params_staus, if event works nomarlly then return value as normal_params, if not return the first param that get issue and its issuse such as no_params or null_params
+(
+    select
+        t_final_2.*,
+        {{generate_query_check_event_param(event_name_list_as_dict = event_name_list_as_dict)}} as params_status
+    from t_final_2
+)
+
+{% endmacro -%}
