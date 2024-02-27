3cc3dd93fb794f97fd9e0c9b0df647a995972c53
Phuc Dinh Minh <120212dinhphuc@isneu.org>
2024-02-27 03:05:37 +0000

    upgrade check params in event macro

--- macros/macro_in_model/firebase/check_params_in_events.sql
+++ macros/macro_in_model/firebase/check_params_in_events.sql
@@ -2,13 +2,29 @@
 {% macro generate_query_check_event_param(event_name_list_as_dict) %}
     case
     {%- for item in event_name_list_as_dict -%}
-        {%- for event_name, param_list in item.items() -%}
-            {%- for key_number, key_name in param_list.items() %}
-        when event_name_in_firebase = '{{event_name}}' and {{key_name}} is null then concat('{{key_name}}', '; no_params_key')
-        when event_name_in_firebase = '{{event_name}}' and {{key_name}} like '%unknown%' then concat('{{key_name}}', '; null_params_value')
-            {%-endfor-%}
-        {%-endfor-%}
+        {%- for event_name, param_list in item.items() %}
+        when event_name_in_firebase = '{{event_name}}' 
+        then
+            case when (
+                {%- for key_number, key_name in param_list.items() %}
+                {{key_name}} is not null and {{key_name}} <> 'unknown'
+                {%- if not loop.last %} and {% endif -%}
+                {%-endfor-%}
+            ) then 'normal_params' 
+            else
+                concat(
+                    'no_params_key: ',
+                    {%- for key_number, key_name in param_list.items() %}
+                    if ({{key_name}} is null, '{{key_name}}, ', ''),
+                    {%-endfor%}
+                    '|| null_params_value: ',
+                    {%- for key_number, key_name in param_list.items() %}
+                    if ({{key_name}} like '%unknown%', '{{key_name}}, ', '')
+                    {%- if not loop.last %} , {% endif -%}
+                    {%-endfor%}
+                )
+            end 
+        {%endfor-%}
     {%-endfor%}
-    else 'normal_params' 
     end
     {%- endmacro -%}
